08d4829b4d2cfa5a4b5ba7c47b9c901287e86f0e
Rafal Niesler <rniesler@gmail.com>
2018-07-04 09:54:48 +0200

    Simple suffix trie implementation based on CompressedTrie

---
+++ Algorytmy/src/main/java/suffixtree/SuffixTree.java
@@ -0,0 +1,39 @@
+package suffixtree;
+
+import datastructures.trie.CompressedTrie;
+
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+public class SuffixTree extends CompressedTrie<String, Character> {
+    static class StringSplitter implements Splitter<String, Character> {
+
+        @Override
+        public Iterable<Character> split(String str) {
+            return str.chars().mapToObj(charInt -> Character.valueOf((char) charInt)).collect(Collectors.toList());
+        }
+
+        @Override
+        public String join(Iterable<Character> segments) {
+            return StreamSupport.stream(segments.spliterator(), false)
+                    .collect(StringBuilder::new, (stringBuilder, character) -> stringBuilder.append(character),
+                            (builder, subBuilder) -> builder.append(subBuilder))
+                    .toString();
+        }
+    }
+
+    public SuffixTree() {
+        super(new StringSplitter());
+    }
+
+    public SuffixTree(Iterable<String> values) {
+        super(values, new StringSplitter());
+    }
+
+    @Override
+    public void put(String str) {
+        for (int i = 0; i < str.length(); i++) {
+            super.put(str.substring(i));
+        }
+    }
+}
---
+++ Algorytmy/src/test/groovy/datastructures/graph/SuffixTreeSpecification.groovy
@@ -0,0 +1,31 @@
+package datastructures.graph
+
+import suffixtree.SuffixTree
+import spock.lang.Specification
+
+class SuffixTreeSpecification extends Specification {
+    def 'test contains'() {
+        setup:
+        final suffixTree = new SuffixTree();
+
+        when:
+        suffixTree.put('abc')
+        then:
+        suffixTree.contains('abc')
+        suffixTree.contains('bc')
+        suffixTree.contains('c')
+        !suffixTree.contains('ab')
+        !suffixTree.contains('a')
+
+        when:
+        suffixTree.put('dbc')
+        then:
+        suffixTree.contains('dbc')
+        suffixTree.contains('abc')
+        suffixTree.contains('bc')
+        suffixTree.contains('c')
+        !suffixTree.contains('ab')
+        !suffixTree.contains('a')
+        4 == suffixTree.size()
+    }
+}
