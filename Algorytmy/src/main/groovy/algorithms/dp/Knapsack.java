c1cd2a1275a86f36a4e2c001e7bb11c3cf5df422
Rafal Niesler <rniesler@gmail.com>
2018-08-02 09:48:48 +0200

    Knapsack in 4 versions

--- Algorytmy/src/main/groovy/algorithms/dp/Knapsack.java
+++ Algorytmy/src/main/groovy/algorithms/dp/Knapsack.java
@@ -33,4 +33,54 @@ public class Knapsack {
             return result;
         }
     }
+
+    public static int unboundedKnapsack(final int capacity, final Item[] items) {
+        int[] memo = new int[capacity + 1];
+        return unboundedKnapsackRecursive(capacity, items, memo);
+    }
+
+    private static int unboundedKnapsackRecursive(final int capacity, final Item[] items, final int[] memo) {
+        if (memo[capacity] != 0) {
+            return memo[capacity];
+        } else {
+            int maxValue = 0;
+            for (Item item : items) {
+                if (item.weight <= capacity) {
+                    int value = item.value + unboundedKnapsack(capacity - item.weight, items);
+                    if (value > maxValue) {
+                        maxValue = value;
+                    }
+                }
+            }
+            return maxValue;
+        }
+    }
+
+    public static int unboundedBottomUp(final int capacity, final Item[] items) {
+        int memo[] = new int[capacity + 1];
+        for (int i = 1; i <= capacity; i++) {
+            memo[i] = memo[i - 1];
+            for (Item item : items) {
+                if (item.weight <= i) {
+                    int valueWithItem = item.value + memo[i - item.weight];
+                    memo[i] = Math.max(valueWithItem, memo[i]);
+                }
+            }
+        }
+        return memo[capacity];
+    }
+
+    public static int solveBottomUp(final int capacity, final Item[] items) {
+        int memo[][] = new int[items.length + 1][capacity + 1];
+        for (int item = 0; item < items.length; item++) {
+            for (int subCapacity = 1; subCapacity <= capacity; subCapacity++) {
+                if (items[item].weight <= subCapacity) {
+                    int valueWithItem = items[item].value + memo[item + 1][capacity - items[item].weight];
+                    memo[item + 1][subCapacity] = Math.max(valueWithItem, memo[item][capacity]);
+                }
+            }
+        }
+
+        return memo[items.length][capacity];
+    }
 }
--- Algorytmy/src/test/groovy/algorithms/dp/KnapsackSpecification.groovy
+++ Algorytmy/src/test/groovy/algorithms/dp/KnapsackSpecification.groovy
@@ -16,4 +16,67 @@ class KnapsackSpecification extends Specification {
         expect:
         Knapsack.solve(capacity, items) == 220
     }
+
+    void 'g4g bottom up'() {
+        setup:
+        final items = [
+                new Item(60, 10),
+                new Item(100, 20),
+                new Item(120, 30)
+        ] as Item[]
+        final capacity = 50;
+
+        expect:
+        Knapsack.solveBottomUp(capacity, items) == 220
+    }
+
+    void 'unbounded g4g'() {
+        setup:
+        final items = [
+                new Item(1, 1),
+                new Item(30, 50)
+        ] as Item[]
+        final capacity = 100
+
+        expect:
+        Knapsack.unboundedKnapsack(capacity, items) == 100
+    }
+
+    void 'unbounded g4g 2'() {
+        setup:
+        final items = [
+                new Item(10, 1),
+                new Item(40, 3),
+                new Item(50, 4),
+                new Item(70, 5)
+        ] as Item[]
+        final capacity = 8
+        expect:
+        Knapsack.unboundedKnapsack(capacity, items) == 110
+    }
+
+    void 'unbounded g4g bottom up'() {
+        setup:
+        final items = [
+                new Item(1, 1),
+                new Item(30, 50)
+        ] as Item[]
+        final capacity = 100
+
+        expect:
+        Knapsack.unboundedBottomUp(capacity, items) == 100
+    }
+
+    void 'unbounded g4g bottom up 2'() {
+        setup:
+        final items = [
+                new Item(10, 1),
+                new Item(40, 3),
+                new Item(50, 4),
+                new Item(70, 5)
+        ] as Item[]
+        final capacity = 8
+        expect:
+        Knapsack.unboundedBottomUp(capacity, items) == 110
+    }
 }
