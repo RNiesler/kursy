f70ffa115b1e8387a1aaeadb27b22e9ce4bc1642
Rafal Niesler <rniesler@gmail.com>
2018-08-08 22:17:30 +0200

    Coin Game strategy with dynamic programming

--- Algorytmy/src/main/java/tasks/g4g/CoinGameStrategy.java
+++ Algorytmy/src/main/java/tasks/g4g/CoinGameStrategy.java
@@ -3,7 +3,35 @@ package tasks.g4g;
 public class CoinGameStrategy {
     public static int maxWin(int[] coins) {
         int[][] memo = new int[coins.length][coins.length];
-        return maxWinRecursive(coins, 0, coins.length - 1, true)[0];
+        return maxWinDynamic(coins, 0, coins.length - 1, memo);
+    }
+
+    private static int maxWinDynamic(int[] coins, int first, int last, int[][] memo) {
+        if (first > last) {
+            return 0;
+        } else if (first == last) {
+            return coins[first];
+        } else if (memo[first][last] != 0) {
+            return memo[first][last];
+        } else {
+            // we choose first
+            int maxWithFirst = coins[first] + Math.min(
+                    // opponent chooses first as well
+                    maxWinDynamic(coins, first + 2, last, memo),
+                    //opponent chooses last
+                    maxWinDynamic(coins, first + 1, last - 1, memo)
+            );
+            // we choose last
+            int maxWithLast = coins[last] + Math.min(
+                    // opponent chooses first
+                    maxWinDynamic(coins, first + 1, last - 1, memo),
+                    // opponent chooses last as well
+                    maxWinDynamic(coins, first, last - 2, memo)
+            );
+            int result = Math.max(maxWithFirst, maxWithLast);
+            memo[first][last] = result;
+            return result;
+        }
     }
 
     private static int[] maxWinRecursive(int[] coins, int first, int last, boolean firstPlayer) {
